492a054982e00fec6fc57788730a7bdafdd82cf6
saniok6 <67289919+saniok6@users.noreply.github.com>
2020-07-11 12:10:46 +0300

    Run test with coverage
    func37 and func40 coverage 100%

--- __tests__/func37.spec.ts
+++ __tests__/func37.spec.ts
@@ -7,4 +7,7 @@ describe("Test cases for Power1 function", () => {
     test("Should return A in power B with result 16 if A = 2 and B = 4", () => {
         expect(Power1(2,4)).toBe(16);
     });
+    test("Should return 0 if A = 0", () => {
+        expect(Power1(0,4)).toBe(0);
+    });
 })
--- __tests__/func40.spec.ts
+++ __tests__/func40.spec.ts
@@ -1,8 +1,11 @@
 import {Exp1} from "../src/problems/func40";
 
-describe("Test cases for Power1 function", () => {
-    test("Should return an approximate value ", () => {
+describe("Test cases for Exp1 function", () => {
+    test("Should return 30 an approximate values of the function exp(x) at a given point 4 for six given 6 ", () => {
         expect(Exp1(4,6)).toBeGreaterThan(30);
         expect(Exp1(4,6)).toBeLessThan(31);
+    });
+    test("Should return Is equal or less than 0 if e equal or less than 0 ", () => {
+        expect(Exp1(4,0)).toBe('Is equal or less than 0');
     });
 })
